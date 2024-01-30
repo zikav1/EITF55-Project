c4b391e02d00b2df9ee28cbc9c2ba1f254a35d16
zikav1 <hugo@rolfs.me>
2024-01-30 22:23:16 +0100

    Vet inte ens vad jag håller på med här

--- Project-1/Rabin-Miller.py
+++ Project-1/Rabin-Miller.py
@@ -4,28 +4,50 @@ class RabinMiller:
 
     def __init__(self) -> None:
         pass
-
+    
     # Checking if the number is prime according to Rabin-Miller's algorithm
     # Tests if it's not prime, if it's composit
     # Say our n = 53
     def rabin_miller_is_prime(self, n):
+        if n <= 1 or n % 2 == 0:
+            return False
         
+        N = n # Saving the original "n" value
         n = n - 1
-        k , m = 1 , 0
-
+        k = 1
+        found = False
 
         while n % pow(2, k) == 0:
             k += 1
 
         
         k = k - 1 # We need the prev k val
-        m += n // pow(2, k)
+        m = n // pow(2, k)
 
-        # now lets find the a. condition for a: 1 < a < n - 1
-        # a = random.randint(2, n - 1)
+        # now lets find the a. condition for a: 1 < a < n - 1.
         a = 2
-        b = pow(a, m) % (n + 1)
-        return b
+
+        b = pow(a, m, N)
+        
+        if b == 1:
+            return False
+
+        while not found:
+            b = pow(b, 2, N)
+            b = abs(b)
+
+            if b == N - 1:
+                found = True
+            
+        
+        if b == N - 1:
+            return True
+
+        
+        return False
+    
+
+        
 
 
     
@@ -33,21 +55,8 @@ class RabinMiller:
 
 
 
-
-
-
-
-        
-
-
-
-
-
-
-
-
 rabin_miller = RabinMiller()
-print(rabin_miller.rabin_miller_is_prime(53))
+print(rabin_miller.rabin_miller_is_prime(547381))
 
     
 
